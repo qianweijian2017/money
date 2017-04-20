7c4a2286e300e379795e05c6d3eead272434fa1b
qianweijian <315905217@qq.com>
2017-04-20 18:52:48 +0800

    添加分页样式,添加控制器添加数据的方法-----for钱伟建

--- Application/Home/Controller/FundController.class.php
+++ Application/Home/Controller/FundController.class.php
@@ -13,15 +13,84 @@ class FundController extends Controller
 	 * @return [string] [分页的字符串]
 	 */
 	public function fundlist(){  
-		$fund = M('fund'); // 实例化Data数据对象  date 是你的表名 
-    	$count=$fund->where($where)->count(); //where 为条件,可作分类分页
-    	$page=new Page($count,$limit);	//count总页数,limit是显示的行数
+		$fund = M('fund'); // 实例化Data数据对象  date 是你的表名  
+		$where="1=1";
+    	$count=$fund->where($where)->count(); //where 为条件,可作分类分页 
+    	$page=new Page($count,8);	//count总页数,limit是显示的行数
     	$fundlist=$fund->where($where)->order('fund_code desc')->limit($page->firstRow.',8' )->select();
     	$sPages=$page->show();
    	 	$this->assign('fundlist',$fundlist);// 赋值数据集
     	$this->assign('sPages',$sPages);// 赋值分页输出
     	$this->display(); // 输出模板 
 	}
-	
- 
+	/**
+	 * 获取数据
+	 * @param  int $num  获取的条数
+	 * @param  object $fund 对象 
+	 * @return boolean 成功信息
+	 */
+	 public function getFundData($num='',$fund){
+	 			$array=array();
+				//得到网页数据
+			    $json = file_get_contents('http://www.gffunds.com.cn/apistore/JsonService?service=BaseInfo&method=Fund&op=queryFundByGFCategory');
+				//把json转换成数组
+			    $text = json_decode($json, true);
+				//需要抽取的字段
+			    $data = ['FUNDCODE',
+			        'FUNDFULLNAME',
+			        'FUNDTYPEGF',
+			        'FUNDNAME',
+			        'WEBPRODUCTNAME',
+			        'YIELDTHISY',
+			        'NAVUNIT',
+			        'DAYINCREMENTRATE',
+			        'NAVACCUMULATED',
+			        'FUNDSTATUS',
+			        'CREATEDATEFA'];
+			      $col=['fund_code',
+			        'fund_fullname',
+			        'fund_type',
+			        'fund_name',
+			        'web_productname',
+			        'year_profit',
+			        'navunit',
+			        'day_up',
+			        'threemouth_up',
+			        'fund_status',
+			        'create_time' 
+			      ];
+			    if ($num == "") {
+			        $num = $text['totalrows'];
+			    } 
+			    for ($i = 0; $i < $num; $i++) { 
+			        $str = "";//新建空字符串 
+			        $douhao = '';
+
+			        //循环抽取的数组
+
+			        foreach ($data as $val) {
+
+			            //将一个数组的值拼接成字符串
+
+			            $str .= $douhao . $text['data'][$i][$val];
+
+			            $douhao = ',';
+			        }
+			        //将字符串转化为新数组
+
+			        $pieces = explode(",", $str);
+			         
+			        $array[]=$pieces; 
+
+			    } 
+
+			    foreach ($array as $key) {
+			    	foreach ($key as $k => $value) {
+			     			 $a[$col[$k]]=$value;
+			     		}
+			     	$fund->add($a); 
+			    } 
+			    return true;
+
+	}
 }
--- ThinkPHP/Library/Org/Util/page.class.php
+++ ThinkPHP/Library/Org/Util/page.class.php
@@ -2,147 +2,144 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2009 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// |         lanfengye <zibin_5257@163.com>
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
+/*
+ * PHP分页类
+ * 修改者：白俊遥
+ * 日  期：2015.5.10
+ * 邮  箱：baijunyao@baijunyao.com
+ * 博  客：http://www.baijunyao.com
+ */
 namespace Org\Util;
-class Page {
-    
-    // 分页栏每页显示的页数
-    public $rollPage = 5;
-    // 页数跳转时要带的参数
-    public $parameter  ;
-    // 分页URL地址
-    public $url     =   '';
-    // 默认列表每页显示行数
-    public $listRows = 20;
-    // 起始行数
-    public $firstRow    ;
-    // 分页总页面数
-    protected $totalPages  ;
-    // 总行数
-    protected $totalRows  ;
-    // 当前页数
-    protected $nowPage    ;
-    // 分页的栏的总页数
-    protected $coolPages   ;
+
+class Page{
+    public $firstRow; // 起始行数
+    public $listRows; // 列表每页显示行数
+    public $parameter; // 分页跳转时要带的参数
+    public $totalRows; // 总行数
+    public $totalPages; // 分页总页面数
+    public $rollPage   = 5;// 分页栏每页显示的页数
+    public $lastSuffix = true; // 最后一页是否显示总页数
+
+    private $p       = 'p'; //分页参数名
+    private $url     = ''; //当前链接URL
+    private $nowPage = 1;
+
     // 分页显示定制
-    protected $config  =    array('header'=>'条记录','prev'=>'上一页','next'=>'下一页','first'=>'第一页','last'=>'最后一页','theme'=>' %totalRow% %header% %nowPage%/%totalPage% 页 %upPage% %downPage% %first%  %prePage%  %linkPage%  %nextPage% %end%');
-    // 默认分页变量名
-    protected $varPage;
+    private $config  = array(
+        'header' => '<span class="rows">共 %TOTAL_ROW% 条记录</span>',
+        'first'   => '首页',
+        'prev'   => '上一页',
+        'next'   => '下一页',
+        'last'   => '末页',
+        'theme'  => '%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%',
+    );
 
     /**
      * 架构函数
-     * @access public
      * @param array $totalRows  总的记录数
      * @param array $listRows  每页显示记录数
      * @param array $parameter  分页跳转的参数
      */
-    public function __construct($totalRows,$listRows='',$parameter='',$url='') {
-        $this->totalRows    =   $totalRows;
-        $this->parameter    =   $parameter;
-        $this->varPage      =   C('VAR_PAGE') ? C('VAR_PAGE') : 'p' ;
-        if(!empty($listRows)) {
-            $this->listRows =   intval($listRows);
-        }
-        $this->totalPages   =   ceil($this->totalRows/$this->listRows);     //总页数
-        $this->coolPages    =   ceil($this->totalPages/$this->rollPage);
-        $this->nowPage      =   !empty($_GET[$this->varPage])?intval($_GET[$this->varPage]):1;
-        if(!empty($this->totalPages) && $this->nowPage>$this->totalPages) {
-            $this->nowPage  =   $this->totalPages;
-        }
-        $this->firstRow     =   $this->listRows*($this->nowPage-1);
+    public function __construct($totalRows, $listRows=20, $parameter = array()) {
+        C('VAR_PAGE') && $this->p = C('VAR_PAGE'); //设置分页参数名称
+        /* 基础设置 */
+        $this->totalRows  = $totalRows; //设置总记录数
+        $this->listRows   = $listRows;  //设置每页显示行数
+        $this->parameter  = empty($parameter) ? $_GET : $parameter;
+        $this->nowPage    = empty($_GET[$this->p]) ? 1 : intval($_GET[$this->p]);
+        $this->nowPage    = $this->nowPage>0 ? $this->nowPage : 1;
+        $this->firstRow   = $this->listRows * ($this->nowPage - 1);
     }
 
+    /**
+     * 定制分页链接设置
+     * @param string $name  设置名称
+     * @param string $value 设置值
+     */
     public function setConfig($name,$value) {
         if(isset($this->config[$name])) {
-            $this->config[$name]    =   $value;
+            $this->config[$name] = $value;
         }
     }
 
     /**
-     * 分页显示输出
-     * @access public
+     * 生成链接URL
+     * @param  integer $page 页码
+     * @return string
+     */
+    private function url($page){
+        return str_replace(urlencode('[PAGE]'), $page, $this->url);
+    }
+
+    /**
+     * 组装分页链接
+     * @return string
      */
     public function show() {
         if(0 == $this->totalRows) return '';
-        $p              =   $this->varPage;
-        $nowCoolPage    =   ceil($this->nowPage/$this->rollPage);
 
-        // 分析分页参数
-        if($this->url){
-            $depr       =   C('URL_PATHINFO_DEPR');
-            $url        =   rtrim(U('/'.$this->url,'',false),$depr).$depr.'__PAGE__';
-        }else{
-            if($this->parameter && is_string($this->parameter)) {
-                parse_str($this->parameter,$parameter);
-            }elseif(empty($this->parameter)){
-                unset($_GET[C('VAR_URL_PARAMS')]);
-                if(empty($_GET)) {
-                    $parameter  =   array();
-                }else{
-                    $parameter  =   $_GET;
-                }
-            }
-            $parameter[$p]  =   '__PAGE__';
-            $url            =   U('',$parameter);
-        }
-        //上下翻页字符串
-        $upRow          =   $this->nowPage-1;
-        $downRow        =   $this->nowPage+1;
-        if ($upRow>0){
-            $upPage     =   "<li><a href='".str_replace('__PAGE__',$upRow,$url)."'>".$this->config['prev']."</a></li>";
-        }else{
-            $upPage     =   '';
+        /* 生成URL */
+        $this->parameter[$this->p] = '[PAGE]';
+        $this->url = U(ACTION_NAME, $this->parameter);
+        /* 计算分页信息 */
+        $this->totalPages = ceil($this->totalRows / $this->listRows); //总页数
+        if(!empty($this->totalPages) && $this->nowPage > $this->totalPages) {
+            $this->nowPage = $this->totalPages;
         }
 
-        if ($downRow <= $this->totalPages){
-            $downPage   =   "<li><a href='".str_replace('__PAGE__',$downRow,$url)."'>".$this->config['next']."</a></li>";
-        }else{
-            $downPage   =   '';
-        }
-        // << < > >>
-        if($nowCoolPage == 1){
-            $theFirst   =   '';
-            $prePage    =   '';
-        }else{
-            $preRow     =   $this->nowPage-$this->rollPage;
-            $prePage    =   "<li><a href='".str_replace('__PAGE__',$preRow,$url)."' >上".$this->rollPage."页</a></li>";
-            $theFirst   =   "<li><a href='".str_replace('__PAGE__',1,$url)."' >".$this->config['first']."</a></li>";
-        }
-        if($nowCoolPage == $this->coolPages){
-            $nextPage   =   '';
-            $theEnd     =   '';
-        }else{
-            $nextRow    =   $this->nowPage+$this->rollPage;
-            $theEndRow  =   $this->totalPages;
-            $nextPage   =   "<li><a href='".str_replace('__PAGE__',$nextRow,$url)."' >下".$this->rollPage."页</a></li>";
-            $theEnd     =   "<li><a href='".str_replace('__PAGE__',$theEndRow,$url)."' >".$this->config['last']."</a></li>";
-        }
-        // 1 2 3 4 5
-        $linkPage = "";
-        for($i=1;$i<=$this->rollPage;$i++){
-            $page       =   ($nowCoolPage-1)*$this->rollPage+$i;
-            if($page!=$this->nowPage){
-                if($page<=$this->totalPages){
-                    $linkPage .= "&nbsp;<li><a href='".str_replace('__PAGE__',$page,$url)."'>&nbsp;".$page."&nbsp;</a></li>";
-                }else{
-                    break;
-                }
+        /* 计算分页零时变量 */
+        $now_cool_page      = $this->rollPage/2;
+        $now_cool_page_ceil = ceil($now_cool_page);
+
+        //上一页
+        $up_row  = $this->nowPage - 1;
+        $up_page = $up_row > 0 ? '<a class="prev" href="' . $this->url($up_row) . '">' . $this->config['prev'] . '</a>' : '<a class="prev" href="javascript:;">' . $this->config['prev'] . '</a>';
+
+        //下一页
+        $down_row  = $this->nowPage + 1;
+        $down_page = ($down_row <= $this->totalPages) ? '<a class="next" href="' . $this->url($down_row) . '">' . $this->config['next'] . '</a>' : '<a class="next" href="javascript:;">' . $this->config['next'] . '</a>';
+
+        //第一页
+        $the_first = '<a class="first" href="' . $this->url(1) . '">' . $this->config['first'] . '</a>';
+
+        //最后一页
+        $the_end = '<a class="end" href="' . $this->url($this->totalPages) . '">' . $this->config['last'] . '</a>';
+
+        //数字连接
+        $link_page = "";
+        for($i = 1; $i <= $this->rollPage; $i++){
+            if(($this->nowPage - $now_cool_page) <= 0 ){
+                $page = $i;
+            }elseif(($this->nowPage + $now_cool_page - 1) >= $this->totalPages){
+                $page = $this->totalPages - $this->rollPage + $i;
             }else{
-                if($this->totalPages != 1){
-                    $linkPage .= "&nbsp;<span class='current'>".$page."</span>";
-                }
+                $page = $this->nowPage - $now_cool_page_ceil + $i;
             }
-        }
-        $pageStr     =   str_replace(
-            array('%header%','%nowPage%','%totalRow%','%totalPage%','%upPage%','%downPage%','%first%','%prePage%','%linkPage%','%nextPage%','%end%'),
-            array($this->config['header'],$this->nowPage,$this->totalRows,$this->totalPages,$upPage,$downPage,$theFirst,$prePage,$linkPage,$nextPage,$theEnd),$this->config['theme']);
-        return $pageStr;
-    }
+            if ($page>0) {
+                if($page != $this->nowPage){
+                    if($page <= $this->totalPages){
+                        $link_page .= '<a class="num" href="' . $this->url($page) . '">' . $page . '</a>';
+                    }else{
+                        break;
+                    }
+                }else{
+                    $link_page .= '<span class="current">' . $page . '</span>';
+                }                
+            }
 
+        }
+
+        //替换分页内容
+        $page_str = str_replace(
+            array('%HEADER%', '%NOW_PAGE%', '%UP_PAGE%', '%DOWN_PAGE%', '%FIRST%', '%LINK_PAGE%', '%END%', '%TOTAL_ROW%', '%TOTAL_PAGE%'),
+            array($this->config['header'], $this->nowPage, $up_page, $down_page, $the_first, $link_page, $the_end, $this->totalRows, $this->totalPages),
+            $this->config['theme']);
+        return '<div class="page">'.$page_str.'</div>';
+    }
 }
