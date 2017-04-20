7b21875b00beba290100f306bc12f87de5426638
qianweijian <315905217@qq.com>
2017-04-20 17:06:40 +0800

    删除多余的newsController fundlist方法添加了分页  for 钱伟建

--- Application/Home/Controller/FundController.class.php
+++ Application/Home/Controller/FundController.class.php
@@ -1,18 +1,27 @@
-<?
+<?php
 namespace Home\Controller;
 use Think\Controller; 
+use Org\Util\page;// 导入分页类
 /**
 * 基金超市类
 */
 class FundController extends Controller
-{
-	//CSS样式删除了还可以加载????*{ 12px }怎么来的!
-	function fundlist()
-	{
-		
-		$fund=M("fund");
- 		$fundlist=$fund->select();  
- 		$this->assign("fundlist",$fundlist);
-		$this->display();
+{ 
+	/**
+	 * 显示基金超市列表并分页 
+	 * @return [array] [基金超市数组]
+	 * @return [string] [分页的字符串]
+	 */
+	public function fundlist(){  
+		$fund = M('fund'); // 实例化Data数据对象  date 是你的表名 
+    	$count=$fund->where($where)->count(); //where 为条件,可作分类分页
+    	$page=new Page($count,$limit);	//count总页数,limit是显示的行数
+    	$fundlist=$fund->where($where)->order('fund_code desc')->limit($page->firstRow.',8' )->select();
+    	$sPages=$page->show();
+   	 	$this->assign('fundlist',$fundlist);// 赋值数据集
+    	$this->assign('sPages',$sPages);// 赋值分页输出
+    	$this->display(); // 输出模板 
 	}
+	
+ 
 }
