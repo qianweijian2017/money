8a906fb83ce85f18d7a9f49e55bdfa883493ceb8
qianweijian <315905217@qq.com>
2017-04-22 02:14:41 +0800

    Fund基金超市动态列表调用完成

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -4,6 +4,15 @@ function is_active($this_control,$menu_name)
 	 if($this_control==$menu_name){
 	 	echo "active";
 	 }else{
-	 	return ;
+	 	return;
 	 }
+}
+function select_active($this_select,$current_type='')
+{ 
+	 
+ 	if($this_select==$current_type){
+ 		echo "active";
+	}else{
+	 	return;
+	} 
 }
--- Application/Home/Controller/FundController.class.php
+++ Application/Home/Controller/FundController.class.php
@@ -12,17 +12,31 @@ class FundController extends Controller
 	 * @return [array] [基金超市数组]
 	 * @return [string] [分页的字符串]
 	 */
-	public function fundlist(){  
-		$fund = M('fund'); // 实例化Data数据对象  date 是你的表名  
-		$where="1=1";
-    	$count=$fund->where($where)->count(); //where 为条件,可作分类分页 
-    	$page=new Page($count,8);	//count总页数,limit是显示的行数
-    	$fundlist=$fund->where($where)->order('year_profit desc')->limit($page->firstRow.',8' )->select();
-    	$sPages=$page->show();
-   	 	$this->assign('fundlist',$fundlist);// 赋值数据集
-    	$this->assign('sPages',$sPages);// 赋值分页输出 
+	public function fundmain(){  
+		$fund = M('fund'); 
+		$fundmain=$fund->order('year_profit desc')->limit(0,8)->select();
+   	 	$this->assign('fundmain',$fundmain);// 赋值数据集
     	$this->display(); // 输出模板 
 	}
+	public function fundlist(){
+		$fund = M('fund'); // 实例化fund数据对象  fund 是你的表名   
+		$where=I("get.ftype")=="2"?"1=1":$this->getWhere(I("get.ftype"));   
+    	$count=$fund->where($where)->count(); //where 为条件,可作分类分页 
+    	$sPages="";							//定义分页
+    	if($count>8){ 
+	    	$page=new Page($count,8);	//count总页数,limit是显示的行数 
+	    	$sPages=$page->show();
+    	}
+    	//获取列表
+    	$fundlist=$fund->where($where)->order('year_profit desc')->limit($page->firstRow.',8' )->select();
+    	$this->assign('fundlist',$fundlist);// 赋值数据集
+    	$this->assign('sPages',$sPages);// 赋值分页输出  
+		$this->display(); // 输出模板 
+	}
+	public function getWhere($fund_type){ 
+		$where="fund_type='{$fund_type}'";
+	   	return $where;
+	} 
 
 	public function detail(){ 
 
