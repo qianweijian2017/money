766d76be0f0316a63eef8a69024223ce4032bcc2
qianweijian <315905217@qq.com>
2017-04-21 21:56:57 +0800

    添加导航固定 js

--- Public/Home/js/base.js
+++ Public/Home/js/base.js
@@ -1,25 +1,26 @@
 ;(function () {
     //导航栏，选中展开
-    $('[hover="1"]').mouseenter(function(){
-        $(this).addClass('open');
-    }).mouseleave(function () {
-        $(this).removeClass('open');
-    });
+    // $('[hover="1"]').mouseenter(function(){
+    //     $(this).addClass('open');
+    // }).mouseleave(function () {
+    //     $(this).removeClass('open');
+    // });
 
-    $('.nav_title li').mouseenter(function () {
-        $(this).find(".icon_180").css('transform','rotate(180deg)');
-    }).mouseleave(function () {
-        $(this).find(".icon_180").css('transform','rotate(0deg)');
-    });
+    // $('.nav_title li').mouseenter(function () {
+    //     $(this).find(".icon_180").css('transform','rotate(180deg)');
+    // }).mouseleave(function () {
+    //     $(this).find(".icon_180").css('transform','rotate(0deg)');
+    // });
 //    滚动屏幕定位
-    var nav_top=$('.fixe_nav').offset().top;
-    $(document).scroll(function () {
+    var nav_top=$('nav').offset().top;
+    $(window).scroll(function () {
         var scroll_top=$(this).scrollTop();
         if(nav_top<scroll_top){
-            $(".fixe_nav").css({"position":"fixed","top":0,'padding-top':'10px','z-index':'999'});
-        }else if(nav_top>=scroll_top){
-            $(".fixe_nav").css({"position":"static",'padding-top':'24px','z-index':'999'});
+            $("nav").css({"position":"fixed","top":0,'z-index':'999'});
+        }else if(nav_top >= scroll_top){
+            $("nav").removeProp("style");
         }
     });
+    $(".nav_title li.active").children("a").prop("href","javascript:;")
 
 }());
