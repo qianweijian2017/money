599abff8350883d6604dcf599352a05db6d0e9fc
LiangZiQ <1729333858@qq.com>
2017-04-26 21:12:24 +0800

    关于帮助中心的

---
+++ Application/Home/Controller/HelpController.class.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Home\Controller;
+use Think\Controller;
+
+class HelpController extends Controller
+{
+    public function helpcenter()
+    {
+        $this->display();
+    }
+}
