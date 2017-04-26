7f20932de6f6bedf5371b8c5038c36ab143e88b5
LiangZiQ <1729333858@qq.com>
2017-04-26 21:14:53 +0800

    关于信息披露的

---
+++ Application/Home/Controller/MessageController.class.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Home\Controller;
+use Think\Controller;
+
+class MessageController extends Controller
+{
+    public function information()
+    {
+        $this->display();
+    }
+}
