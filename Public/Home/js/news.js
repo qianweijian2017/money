a1438dd4f3c8e859d5f48cc805ba0c38d0d86294
Your Name <you@example.com>
2017-04-28 10:52:49 +0800

    添加bodyli

--- Public/Home/js/news.js
+++ Public/Home/js/news.js
@@ -1,12 +1,35 @@
 $(document).ready(function () {
     $("#title li").each(function () {
         $("#title li").eq(0).addClass("title");
-        $(this).on("click",function () {
+        $(this).on("click", function () {
             $(this).addClass("title").siblings().removeClass("title")
             $(this).mouseout(function () {
                 $(this).addClass("title_out")
             })
         });
 
+
     });
-})
+
+    $('.main-con ul li').on('mouseenter', function () {
+        $(this).stop().animate({
+            opacity: 1,
+            top:"10px"
+        });
+
+    }).on('mouseout', function () {
+        $(this).stop().animate({
+            top:"0px"
+        });
+    });
+    $('.main-con ul li').children().on('mouseenter', function () {
+        $(this).parent().stop().animate({
+            opacity: 1,
+            top:"10px"
+        })
+    }).on('mouseout', function () {
+        $(this).stop().animate({
+            top:"0px"
+        });
+    });
+});
