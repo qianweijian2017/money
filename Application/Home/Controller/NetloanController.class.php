847d69e961cbffec018da95513ecefe0e65daba3
qianweijian <315905217@qq.com>
2017-05-04 22:30:28 +0800

    上传网袋理财,定期理财

---
+++ Application/Home/Controller/NetloanController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Home\Controller;
+use Think\Controller;
+
+class NetloanController extends Controller
+{
+    public function net()
+    {
+        $this->display();
+    }
+}
---
+++ Application/Home/Controller/RegularController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Home\Controller;
+use Think\Controller;
+
+class RegularController extends Controller
+{
+    public function regular()
+    {
+        $this->display();
+    }
+}
