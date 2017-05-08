f0ad086c53d43f6bf55a8eb10b29f81d4f3f41dd
qianweijian <315905217@qq.com>
2017-05-08 19:47:51 +0800

    修改基金控制器

--- Application/Home/Controller/FundController.class.php
+++ Application/Home/Controller/FundController.class.php
@@ -14,30 +14,40 @@ class FundController extends Controller
 	 */
 	public function fundmain(){   
 		$fund = M('fund'); 
-		$fundmain=$fund->order('year_profit desc')->limit(0,8)->select();
+		$fundmain=$fund -> alias('f')
+						-> join('__FUND_TYPE__ t on t.type_id =f.fund_type','left')
+						-> order('year_profit desc') 
+						-> limit(0,8) 
+						-> select();
    	 	$this->assign('fundmain',$fundmain);// 赋值数据集
     	$this->display(); // 输出模板 
 	}
 	public function fundlist(){  
 		$fund = M('fund'); // 实例化fund数据对象  fund 是你的表名   
-		$where=I("get.ftype")=="2"?"1=1":$this->getWhere(I("get.ftype"));   
-    	$count=$fund->where($where)->count(); //where 为条件,可作分类分页   
+		$where=I("get.ftype")=="9"?"1=1":$this->getWhere(I("get.ftype"));   
+    	$count=$fund ->alias('f')
+    				 -> where($where) 
+    				 -> join('__FUND_TYPE__ t on t.type_id =f.fund_type','left')
+    				 -> count(); //where 为条件,可作分类分页   
     	$sPages="";							//定义分页
     	if($count>8){ 
 	    	$page=new Page($count,8);	//count总页数,limit是显示的行数 
 	    	$sPages=$page->show();
     	}
     	//获取列表
-    	$fundlist=$fund->where($where)
-				    	->order('year_profit+0 desc')
-				    	->limit($page->firstRow.',8' )
-				    	->select();
+    	$fundlist=$fund ->  alias('f')
+    					->  join('__FUND_TYPE__ t on t.type_id =f.fund_type','left')
+    					->  where($where)  
+				    	->	order('year_profit+0 desc')
+				    	->	limit($page->firstRow.',8' )
+				    	->	select();
     	$this->assign('fundlist',$fundlist);// 赋值数据集
     	$this->assign('sPages',$sPages);// 赋值分页输出  
 		$this->display(); // 输出模板 
 	}
-	public function getWhere($fund_type){ 
-		$where="fund_type='{$fund_type}'";
+	public function getWhere($fund_type){  
+		$where="type_id = $fund_type"; 
+		
 	   	return $where;
 	} 
 
