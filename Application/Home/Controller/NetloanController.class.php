fe70bba41649ba1ae4f00e30c2f068f21a469377
qianweijian <315905217@qq.com>
2017-05-09 18:46:45 +0800

    添加

--- Application/Home/Controller/NetloanController.class.php
+++ Application/Home/Controller/NetloanController.class.php
@@ -10,10 +10,10 @@ class NetloanController extends Controller
     {
     	$project  	  =  M('project');
     	$where        =  I('get.type',0) == 0 ? : 'proj_type='.I('get.type');
-        $order        =  I('get.sort',0) == 0 ? : $this -> gerSort(I('get.sort'));  
-        $count        =  $project -> where($where) -> count();
-        $page         =  new \Org\Util\page($count,8);  
-        $show         =  $page   -> show();
+      $order        =  I('get.sort',0) == 0 ? : $this -> gerSort(I('get.sort'));  
+      $count        =  $project -> where($where) -> count();
+      $page         =  new \Org\Util\page($count,8);  
+      $show         =  $page   -> show();
     	$projectlist  =	 $project -> alias('p')
     							  -> join('__PROJECT_TYPE__ t on t.type_id = p.proj_type','right')
     							  -> join('__PROJECT_LOCK__ l on l.type_id = t.type_id','left')  
@@ -27,8 +27,8 @@ class NetloanController extends Controller
                                   -> count();  
         $this -> assign('heartCount',$heartCount);
         $this -> assign('mountCount',$mountCount);
-    	$this -> assign('page',$show);
-    	$this -> assign('projectlist',$projectlist);
+    	  $this -> assign('page',$show);
+    	  $this -> assign('projectlist',$projectlist);
         $this -> display();
     }
 
