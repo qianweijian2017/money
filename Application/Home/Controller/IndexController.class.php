ca9e1cb00b498fcacf86211db7aa80baf67225fa
qianweijian <315905217@qq.com>
2017-05-11 15:43:16 +0800

    liweijian

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,16 +7,24 @@ use Think\Controller;
 
 class IndexController extends Controller
 {
+
+
     public function index()
-    {   $M = M();
+    {
+        $M = M();
+//        计算投资百分比
         $sql = "select *,(`proj_amount`*1.0/`proj_total`) as result from mn_project ";
+//        计算剩余可借百分比
+        $sql_lent = "select *,(`proj_total`-`proj_amount`*1.0) as proj_lent from mn_project ";
         $res = $M->query($sql);     //主要针对查询  //返回一个维数组
+        $res_lent = $M->query($sql_lent);     //主要针对查询  //返回一个维数组
         //省心宝
         $shengxinbao = M('project');
-        for($i=0;$i<count($res);$i++)
-        {
-            $data['result']=$res[$i]['result'];
-            $shengxinbao->where("id=".($i+1))->setField('result',$data['result']);
+        for ($i = 0; $i < count($res); $i++) {
+            $data['result'] = $res[$i]['result'];
+            $data['proj_lent'] = $res_lent[$i]['proj_lent'];
+            $shengxinbao->where("id=" . ($i + 1))->setField('result', $data['result']);
+            $shengxinbao->where("id=" . ($i + 1))->setField('proj_lent', $data['proj_lent']);
 
         }
         $data_sxb = $shengxinbao->table('mn_project_lock a,mn_project b')->where('b.proj_type=1 and a.type_id=b.proj_type')->order('result desc')->select();
@@ -33,10 +41,9 @@ class IndexController extends Controller
 
         //月月增
         $yueyuezneg = M('project');
-        for($i=0;$i<count($res);$i++)
-        {
-           $data['result']=$res[$i]['result'];
-           $yueyuezneg->where("id=".($i+1))->setField('result',$data['result']);
+        for ($i = 0; $i < count($res); $i++) {
+            $data['result'] = $res[$i]['result'];
+            $yueyuezneg->where("id=" . ($i + 1))->setField('result', $data['result']);
 
         }
 
@@ -46,19 +53,30 @@ class IndexController extends Controller
         $this->assign('data_yyz_total', $data_yyz[1]);// 赋值数据集
         $this->assign('data_yyz_thr', $data_yyz[2]);// 赋值数据集
 
-         
-        $ppt_info=M('ppt')
-                 ->select();
-                 /*print_r($ppt_info);*/
-                 $this->assign('ppt_info',$ppt_info);
-        $cont=count($ppt_info);
-             $this->assign('cont',$cont); 
 
+        $ppt_info = M('ppt')
+            ->select();
+        /*print_r($ppt_info);*/
+        $this->assign('ppt_info', $ppt_info);
+        $cont = count($ppt_info);
+        $this->assign('cont', $cont);
+
+
+
+
+//        更改项目状态
+        if (IS_POST) {
+            $ID = I("post.ID");
+            $STATUS = I("post.STATUS");
+            $project = M('project');
+            if ($STATUS == 1) {
+                $project->where('id='.$ID)->setField('status', "2");
+            }
+        }
 
         $this->display('./index');
 
     }
-
 
 
 }
--- Public/Home/js/base.js
+++ Public/Home/js/base.js
@@ -1,21 +1,79 @@
 ;(function () {
     //初始化首页数据
-    $tar="100%";
-    for ( i=0;i<6;i++){
+    $change_vot = "100%";
+    for (i = 0; i < 6; i++) {
+        //读取每个进度
+        getVal();
+        //循环已经完成投资的项目
+        if ($status == 1) {
+            if ($val == $change_vot){
+                $.post('./index', {
+                    ID: $id,
+                    STATUS: $status
+                }, function (data) {
 
-        $val=$('.progress-val').eq(i).text();
-        $('.progress-ing').eq(i).css({
-            "width":$val
-        });
-        console.log($val);
-        if($val==$tar){
+                });
+            }
+
+
+            //刷新项目进度（贷款状态）
+            getVal();
+            $('.ft').find('.btn').eq(i).attr('href','/money/index.php/Home/Regular/detail/fixid/'+$id+'.html');
+
+        }else if( $status == 2){
+
+            getVal();
+            $('.ft').find('.btn').eq(i).attr('href','/money/index.php/Home/Netloan/detail/fixid/'+$id+'.html');
             $('.ft').find('.btn').eq(i).css({
-                background:"#cccccc",
-                borderColor:"#cccccc"
-            }).text("已购满")
+                background: "#45b8ef",
+                borderColor: "#45b8ef"
+            }).text("立即贷款");
+            $('.yuqi .yuqitext').eq(i).text("预期年化利息");
+
+            $('.yuqi .yuqinum').eq(i).css({
+                color: "#2d6bef"
+            });
+            //当状态二满了的时候
+
+            if ($val_lent == $change_vot){
+
+                $('.ft').find('.btn').eq(i).css({
+                    background: "#cccccc",
+                    borderColor: "#cccccc"
+                }).text("贷款名额已满")
+            }
+
+
         }
+        //刷新项目进程
+        function getVal() {
+            //获取状态
+            $status = $('.bd-row li').eq(i).attr('data-status');
+            //获取id
+            $id = $('.bd-row li').eq(i).attr('data-id');
+            //获取进度
+            if ($status==1){
+
+            }else if($status==2){
+                $('.progress-val').eq(i).parent().css({
+                    display:"none"
+                });
+                $('.progress-val-lent').eq(i).parent().css({
+                    display:"block"
+                });
+
+            }
+            $val = $('.progress-val').eq(i).text();
+            $val_lent = $('.progress-val-lent').eq(i).text();
 
 
+            $('.progress-ing').eq(i).css({
+                width: $val
+            });
+            $('.progress-ing-lent').eq(i).css({
+                width: $val_lent
+            });
+        }
     }
     //导航栏，选中展开
     // $('[hover="1"]').mouseenter(function(){
@@ -30,16 +88,16 @@
     //     $(this).find(".icon_180").css('transform','rotate(0deg)');
     // });
 //    滚动屏幕定位
-    var nav_top=$('nav').offset().top;
+    var nav_top = $('nav').offset().top;
     $(window).scroll(function () {
-        var scroll_top=$(this).scrollTop();
-        if(nav_top<scroll_top){
-            $("nav").css({"position":"fixed","top":0,'z-index':'999'});
-        }else if(nav_top >= scroll_top){
+        var scroll_top = $(this).scrollTop();
+        if (nav_top < scroll_top) {
+            $("nav").css({"position": "fixed", "top": 0, 'z-index': '999'});
+        } else if (nav_top >= scroll_top) {
             $("nav").removeProp("style");
         }
     });
-    $(".nav_title li.active").children("a").prop("href","javascript:;")
+    $(".nav_title li.active").children("a").prop("href", "javascript:;")
 
 
 }());
