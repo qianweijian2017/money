a0a79e29156391920d1f943f561ce8b70a3d38ed
qianweijian <315905217@qq.com>
2017-05-13 17:40:27 +0800

    权限控制

--- Application/Admin/Common/Controller/AdminController.class.php
+++ Application/Admin/Common/Controller/AdminController.class.php
@@ -9,10 +9,23 @@ class AdminController extends Controller
 	
 	 public function _initialize()
 	 {
-	 	echo "1";
-	 	$user	=  session('admin');
+	  
+	 	$user	       		=  session('admin');
 	 	if(!$user){
-	 		$this -> error('请先登陆',U('User/login'));
+	 		$this -> error('请先登陆',U('User/login'));exit;
+	 	}
+	 	$role_id       		=  $user['role_id'];			//获取用户权限ID
+	 	$rbac_role_auths    =  C('RBAC_ROLE_AUTHS');		//获取用户组的权限
+	 	$currenRoleAuth		=  $rbac_role_auths[$role_id];	//获取当前用户的权限
+
+	 	$controller    		=  strtolower(CONTROLLER_NAME);
+	 	$action        		=  strtolower(ACTION_NAME); 
+	 	 
+	 	if($role_id > 1){
+	 		if(!in_array($controller.'/'.$action,$currenRoleAuth) && !in_array($controller.'/*',$currenRoleAuth)){
+	 				$this -> error('您没有权限',"",3);exit();
+	 		}
+
 	 	}
 	 }
 }
