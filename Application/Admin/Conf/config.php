06fc68849113d4ee787a990d42d254c489258040
qianweijian <315905217@qq.com>
2017-05-13 13:40:09 +0800

    RBAC匹配

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'RBAC_ROLES'  		=> array(
+								1  =>  '超级管理员',
+								2  =>  '前台管理员',
+								3  =>  '后台管理员'
+		),
+	'RBAC_ROLE_AUTHS'  	=> array(
+								1  =>  '*/*',
+								2  =>  array('nav/*','news/*'),
+								3  =>  array('fund/*','project/*','user/*') 
+		),
+	'SHOW_PAGE_TRACE'	=> true,
 );
